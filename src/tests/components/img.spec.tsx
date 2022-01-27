e6a552679130283b34f43bde867e0b8470312c5a
gabriel <gabriel.gregorio.1@outlook.com>
2022-01-27 20:58:36 -0300

    Cause redeploy

--- src/tests/components/img.spec.tsx
+++ src/tests/components/img.spec.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Img from '@/base/img';
 
 describe('<Img />', () => {
-  it('should render image', () => {
+  it('should render image in screen', () => {
     render(<Img src="image.png" alt="um texto qualquer" />);
     expect(screen.getByRole('img', { name: 'um texto qualquer' })).toBeInTheDocument();
   });
