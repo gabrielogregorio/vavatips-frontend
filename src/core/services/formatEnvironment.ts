c94b2bd204f35f876d54d0202868d8f134491fe8
gabriel <gabriel.gregorio.1@outlook.com>
2022-03-11 22:57:37 -0300

    Troca google cloud e modo local pelo cloudnary

--- src/core/services/formatEnvironment.ts
+++ src/core/services/formatEnvironment.ts
@@ -1,8 +1,3 @@
-function removeUrlGoogleStorageFromImages(urlImage: string): string {
-  return urlImage.replace('https://storage.googleapis.com/valorant-tips-bucket/', '');
-}
-
 export function formatImage(file: string) {
-  const urlImage = `${process.env.NEXT_PUBLIC_API_HOST}/images/${file}`;
-  return removeUrlGoogleStorageFromImages(urlImage);
+  return file;
 }
