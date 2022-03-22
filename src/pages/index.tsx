2a21a1739e391d8639fbf099029e6c6defdcd1b7
gabriel <gabriel.gregorio.1@outlook.com>
2022-03-21 22:19:12 -0300

    Added static page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import { Breadcrumb } from '@/widgets/breadcrumb';
-import { ErrorMsg } from '@/base/errorMsg';
 import { Footer } from '@/layout/footer';
-import { Loader } from '@/base/loader';
 import { maps } from '@/data/data-valorant';
 import LINKS from '@/data/links.json';
-import { useMaps } from '@/hooks/useMaps';
 import { Title } from '@/base/title';
 import { Layout } from '@/layout/layout';
 import { Navbar } from '@/layout/navbar';
@@ -15,23 +12,12 @@ import { modelNavbarPublic } from '@/schemas/navbar';
 
 const breadcrumbs = [LINKS.inicio, LINKS.Maps];
 
-const Index = () => {
-  const { mapsApi, isLoading, error } = useMaps();
-
+const Index = ({ mapsApi }: { mapsApi: string[] }) => {
   function renderMap() {
-    if (mapsApi.length === 0) {
-      return null;
-    }
-
     return maps().map((map) =>
       mapsApi.includes(map.name) ? (
         <div key={map.id} className="flex flex-col">
-          <ImageCard
-            heightImage="h-40"
-            href={`/agents?map=${map.name}`}
-            srcImage={map.img}
-            titleImage={map.name}
-          />
+          <ImageCard heightImage="h-40" href={`/agents?map=${map.name}`} srcImage={map.img} titleImage={map.name} />
         </div>
       ) : null,
     );
@@ -44,11 +30,8 @@ const Index = () => {
 
       <SubContainer>
         <Title>Escolha um mapa ai parça </Title>
-        <ErrorMsg msg={error} />
-        <Loader active={isLoading} />
-        <div className="grid grid-cols-1 gap-6 pl-1 pr-1 mb-2 sm:grid-cols-4 w-full">
-          {renderMap()}
-        </div>
+
+        <div className="grid grid-cols-1 gap-6 pl-1 pr-1 mb-2 sm:grid-cols-4 w-full">{renderMap()}</div>
       </SubContainer>
 
       <Footer />
@@ -56,3 +39,14 @@ const Index = () => {
   );
 };
 export default Index;
+
+export async function getStaticProps() {
+  const resp = await fetch('https://backend-valorant.herokuapp.com/maps');
+  const mapsApi = await resp.json();
+
+  return {
+    props: {
+      mapsApi: mapsApi.maps,
+    },
+  };
+}
