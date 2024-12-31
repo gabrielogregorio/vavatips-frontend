d9f96ace52ec33b32c0ed447e6057dbef71874e1
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2024-12-31 11:45:16 -0300

    feat: returns jest

--- jest.setup.js
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom/jest-globals';
-import '@testing-library/jest-dom'
-import 'cross-fetch/polyfill'
-
-jest.mock('uuid', () => ({ v7: () => Math.random().toString() }));
