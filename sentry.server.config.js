1f08143b941baa711589612d9ef02e10b10643a9
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2025-01-01 10:47:15 -0300

    feat: remove deprecated config

--- sentry.server.config.js
+++
@@ -1,9 +0,0 @@
-import * as Sentry from '@sentry/nextjs';
-import { TRACE_SAMPLE_RATE_SENTRY } from './constants.sentry';
-
-if (process.env.NEXT_PUBLIC_SENTRY_IS_ENABLED === 'true') {
-  Sentry.init({
-    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-    tracesSampleRate: TRACE_SAMPLE_RATE_SENTRY,
-  });
-}
