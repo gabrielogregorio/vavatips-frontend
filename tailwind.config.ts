3e25a78b797414168ec2f2608235349ce9dbac8b
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2025-01-02 16:21:10 -0300

    fix: tailwind autocomplete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,114 +1,117 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {
-      boxShadow: {
-        md: "0px 2px 6px 0px rgba(0, 0, 0, 0.1);",
+    extend: {},
+    fontFamily: {
+      poppins: ['"Poppins"', 'sans-serif'],
+    },
+
+    boxShadow: {
+      md: '0px 2px 6px 0px rgba(0, 0, 0, 0.1)',
+    },
+
+    borderRadius: {
+      xxs: '2px',
+      xs: '4px',
+      sm: '6px',
+      full: '100%',
+    },
+
+    spacing: {
+      xxs: '2px',
+      xs: '4px',
+      sm: '6px',
+      md: '8px',
+      lg: '12px',
+      xl: '16px',
+
+      '2xl': '20px',
+      '3xl': '24px',
+
+      '4xl': '28px',
+      '5xl': '32px',
+
+      '6xl': '36px',
+      '7xl': '48px',
+    },
+
+    screens: {
+      desktop: '1366px',
+      tabled: '1024px',
+      mobile: '640px',
+    },
+    width: {
+      'content-desktop': '890px',
+    },
+    borderWidth: {
+      DEFAULT: '1px',
+    },
+
+    colors: {
+      primary: {
+        DEFAULT: '#D85F5F',
+        hard: '#E03F3F',
+        soft: '#E07A7A',
       },
-      spacing: {
-        xxs: "2px",
-        xs: "4px",
-        sm: "6px",
-        md: "8px",
-        lg: "12px",
-        xl: "16px",
 
-        "2xl": "20px",
-        "3xl": "24px",
-
-        "4xl": "28px",
-        "5xl": "32px",
-
-        "6xl": "36px",
-        "7xl": "48px",
+      secondary: {
+        DEFAULT: '#3386D3',
+        hard: '#0764BA',
+        soft: '#72A8DB',
       },
 
-      screens: {
-        mobile: "640px",
-        tabled: "1024px",
-        desktop: "1366px",
+      root: {
+        bg: '#F7F7FA',
       },
-      width: {
-        "content-desktop": "890px",
+      content: {
+        fg: {
+          DEFAULT: '#444444',
+          subcontent: '#6A7281',
+          contrast: '#FFFFFF',
+          placeholder: '#99A1AE',
+          disabled: '#848589',
+        },
+        bg: {
+          DEFAULT: '#FFFFFF',
+          disabled: '#F0F3FA',
+          highlight: '#D9D9D9',
+        },
       },
-
-      borderRadius: {
-        xs: "2px",
-        sm: "4px",
-        md: "6px",
-        full: "100%",
+      accent: {
+        radiant: '#FE9E2E',
+        rose: '#D81E65',
+        purple: '#9747FF',
+        pacific: {
+          blue: '#029ABC',
+        },
       },
-
-      colors: {
-        primary: {
-          DEFAULT: "#D85F5F",
-          hard: "#E03F3F",
-          soft: "#E07A7A",
+      feedback: {
+        error: {
+          hard: '#CB4343',
+          soft: '#FCD2D2',
         },
-
-        secondary: {
-          DEFAULT: "#3386D3",
-          hard: "#0764BA",
-          soft: "#72A8DB",
+        success: {
+          hard: '#125134',
+          soft: '#D1E7DD',
         },
-
-        root: {
-          bg: "#F7F7FA",
-        },
-        content: {
-          fg: {
-            DEFAULT: "#444444",
-            subcontent: "#6A7281",
-            contrast: "#FFFFFF",
-            placeholder: "#99A1AE",
-            disabled: "#848589",
-          },
-          bg: {
-            DEFAULT: "#FFFFFF",
-            disabled: "#F0F3FA",
-            highlight: "#D9D9D9",
-          },
-        },
-        accent: {
-          radiant: "#FE9E2E",
-          rose: "#D81E65",
-          purple: "#9747FF",
-          pacific: {
-            blue: "#029ABC",
-          },
-        },
-        feedback: {
-          error: {
-            hard: "#CB4343",
-            soft: "#FCD2D2",
-          },
-          success: {
-            hard: "#125134",
-            soft: "#D1E7DD",
-          },
-        },
-        overlay: {
-          bg: "rgba(0,0,0, 0.6)",
-          "bg-blur": "rgba(26,26,26,0.5)",
-        },
-        border: {
-          DEFAULT: "#BABABA",
-          soft: "#E8E8E8",
-        },
-        neutral: {
-          900: "#111111",
-          800: "#1A1A1A",
-          750: "#383838",
-          650: "#2C2C2C",
-          500: "#6C6C6C",
-          100: "#EFEFEF",
-        },
+      },
+      overlay: {
+        bg: 'rgba(0,0,0, 0.6)',
+        'bg-blur': 'rgba(26,26,26,0.5)',
+      },
+      border: {
+        DEFAULT: '#BABABA',
+        soft: '#E8E8E8',
+      },
+      neutral: {
+        900: '#111111',
+        800: '#1A1A1A',
+        750: '#383838',
+        650: '#2C2C2C',
+        500: '#6C6C6C',
+        100: '#EFEFEF',
       },
     },
   },
