ebe697636ef11a117ff15aae79e60701c804a2fa
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2025-01-08 09:13:12 -0300

    feat: added wrappers to handle cookies

---
+++ src/@types/CustomFieldValues.ts
@@ -0,0 +1,1 @@
+export type CustomFieldValues = { [x: string]: string | number | boolean | string[] | number[] | boolean };
---
+++ src/@types/routeScreenEnum.ts
@@ -0,0 +1,6 @@
+export enum RouteScreensEnum {
+  'dashboard' = '/admin/dashboard',
+  'login' = '/login',
+  'register' = '/register',
+  'root' = '/',
+}
