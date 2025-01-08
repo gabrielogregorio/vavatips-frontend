8b12a3bc14677eb2b4aa070f93f7684c3cf8cb47
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2025-01-08 18:24:58 -0300

    chore: expand map screens and update vscode config

--- src/@types/routeScreenEnum.ts
+++ src/@types/routeScreenEnum.ts
@@ -3,4 +3,9 @@ export enum RouteScreensEnum {
   'login' = '/login',
   'register' = '/register',
   'root' = '/',
+  'posts' = '/admin/posts',
+  'data' = '/admin/data',
+  'createPost' = '/admin/createPost',
+  'suggestions' = '/admin/suggestions',
+  'profile' = '/admin/profile',
 }
